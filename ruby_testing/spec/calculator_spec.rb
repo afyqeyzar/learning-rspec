50b08ae294787d7f4cfe4ba8c465220f167385cd
afyqeyzar <norafyqe@gmail.com>
2022-09-01 18:29:35 +0100

    adding test

--- ruby_testing/spec/calculator_spec.rb
+++ ruby_testing/spec/calculator_spec.rb
@@ -13,4 +13,29 @@ describe Calculator do
     end
   end
 
+  describe "#subtract" do
+    it "subtract two numbers" do
+      calculator = Calculator.new
+      expect(calculator.subtract(7,2).to eql(5))
+    end
+  end
+
+  describe "#multiply" do
+    it "multiply two numbers" do
+      calculator = Calculator.new
+      expect(calculator.multiply(3, 4).to eql(12))
+    end
+
+    it "multiply more than two numbers" do
+      calculator = Calculator.new
+      expect(calculator.multiply(3, 2, 5).to eql(30))
+    end
+  end
+
+  describe "#divide" do
+    it "divides two numbers" do
+      calculator = Calculator.new
+      expect(calculator.divide(4, 2).to eql(2))
+    end
+  end
 end
