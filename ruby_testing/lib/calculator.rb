49f6d644510a60a117f9313526c931ae07e1d9e0
afyqeyzar <norafyqe@gmail.com>
2022-09-01 18:34:33 +0100

    defined methods in calculator.rb

--- ruby_testing/lib/calculator.rb
+++ ruby_testing/lib/calculator.rb
@@ -3,4 +3,18 @@ class Calculator
     # args.is_a? Array
     args.sum
   end
+
+  def subtract(a, b)
+    a - b
+  end
+
+  def multiply(*args)
+    final = 1.0
+    args.each { |i| final *= i }
+    final.to_i
+  end
+
+  def divide(a, b)
+    a / b
+  end
 end
